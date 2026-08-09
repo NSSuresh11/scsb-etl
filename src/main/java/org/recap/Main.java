6fdd2fb57a74b9f82d89946358290d6511492a00
NSSuresh11 <nssuresh.subramanian@gmail.com>
2026-08-09 09:59:54 +0530

    ETL Upgrade

--- src/main/java/org/recap/Main.java
+++ src/main/java/org/recap/Main.java
@@ -2,8 +2,8 @@ package org.recap;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
